fad61efde560c191ca825bcc44ac8ff7021b495c
nataliag409 <natalia212329@gmail.com>
2024-03-20 13:52:46 -0700

    sdfsddafsadfsdafweqdgvcsdx adwedrbfe dwsaxzc

--- Unit2ProjectGutierrezP5/Assets/Scripts/PlayerController.cs
+++ Unit2ProjectGutierrezP5/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        {
+            
+        }
         if (transform.position.x < -10)
         {
             transform.position = new Vector3(-10, transform.position.y, transform.position.z);
