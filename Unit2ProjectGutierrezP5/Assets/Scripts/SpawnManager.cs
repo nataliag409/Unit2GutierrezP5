09a888126499f5c9496195cc3a3e68127d748ab3
nataliag409 <natalia212329@gmail.com>
2024-03-22 13:51:27 -0700

    htgmfgfjhgh
    jhvghcvmh

--- Unit2ProjectGutierrezP5/Assets/Scripts/SpawnManager.cs
+++ Unit2ProjectGutierrezP5/Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalsPrefabs;
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnRandomeAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
